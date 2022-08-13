bcd76e0c3aec8deda5fb18d0355f7fb5ce306b63
Olga <olga.buinova@gmail.com>
2022-08-13 23:45:17 +0300

    Задача

---
+++ Exercise/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует 
+// массив из строк, длина которых меньше либо равна 3 символа.
+
+string[] stringArray = new string [10];
+
+   Console.WriteLine("Введите любые 10 слов через Enter:");
+for (int i = 0; i < stringArray.Length; i++)
+   {
+      stringArray[i]= Console.ReadLine();
+   }
+
